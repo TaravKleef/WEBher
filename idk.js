6f83ff837935f2bcab304552be2b0db312baeb7d
Tara <taravankleef@mbp-van-tara.home>
2019-10-30 00:21:44 +0100

    Toetsing
    Toetsing eindproduct

--- idk.js
+++ idk.js
@@ -1,10 +1,26 @@
-var visual = document.querySelector('#visual');
-var visualdark = document.querySelector('#visualdark');
+var navWerk = document.querySelector('nav>ul>li:nth-of-type(3)');
+var titleWerk = document.getElementById('werk');
+var navOvermij = document.querySelector('nav>ul>li:nth-of-type(2)');
+var titleOvermij = document.getElementById('overmij');
+var navContact = document.querySelector('nav>ul>li:nth-of-type(4)');
+var titleContact = document.getElementById('contact');
 
-function show() {
+navWerk.addEventListener('click', function () {
     console.log('hoi');
-    visual.classList.toggle('show-visualdark');
-    return false;
-}
+    titleWerk.classList.add('vergroot')
+})
 
-visual.addEventListener('click', show)
+navOvermij.addEventListener('click', function () {
+    titleOvermij.classList.add('vergroot')
+})
+
+navContact.addEventListener('click', function () {
+    titleContact.classList.add('vergroot')
+})
+
+//van https://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to
+// werkt alleen eerste keer klikken
+
+window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+});
